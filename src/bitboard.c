6e006e5b7cede8f2a0c3e03ec40f69b7e24be12b
Pieter te Brake <pieterteb@gmail.com>
2025-08-31 19:26:32 +0200

    Created function to print bitboards in a chessboard layout for easy interpretation.

---
+++ src/bitboard.c
@@ -0,0 +1,28 @@
+#include <inttypes.h>
+#include <stdio.h>
+
+#include "bitboard.h"
+#include "types.h"
+
+
+
+void print_bitboard(FILE* stream, const Bitboard bitboard) {
+    for (int rank = RANK_8; rank >= RANK_1; --rank) {
+        fprintf(stream, "%d  ", rank + 1);
+        for (int file = FILE_A; file < FILE_COUNT; ++file) {
+            Bitboard mask = (Bitboard)1 << (DIRECTION_NORTH * rank + DIRECTION_EAST * file);
+            if ((bitboard & mask) == 0)
+                fprintf(stream, " 0");
+            else
+                fprintf(stream, " 1");
+        }
+        fputc('\n', stream);
+    }
+
+    fprintf(stream, "\n    a b c d e f g h\n"
+
+                    "\nDecimal value:     %" PRIu64
+                    "\nHexadecimal value: %#018" PRIX64,
+                    bitboard,
+                    bitboard);
+}
---
+++ src/bitboard.h
@@ -0,0 +1,13 @@
+#ifndef BITBOARD_H
+#define BITBOARD_H
+
+
+#include "types.h"
+
+
+
+void print_bitboard(FILE* stream, const Bitboard bitboard);
+
+
+
+#endif /* BITBOARD_H */

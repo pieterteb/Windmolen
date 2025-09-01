6d8e9c2fe2c26ccbe5ad6c74bbe9389a1904880e
Pieter te Brake <pieterteb@gmail.com>
2025-09-01 21:10:42 +0200

    Added utils.c and utils.h and created count trailing zeroes function.

---
+++ src/utils.c
@@ -0,0 +1,21 @@
+#include "utils.h"
+
+
+
+static inline int ctzll(unsigned long long x) {
+#if defined(__GNUC__) || defined(__clang__)
+    return __builtin_ctzll(x);
+#elif defined(_MSC_VER)
+    unsigned long index;
+    _BitScanForward(&index, x);
+    return (int)index;
+#else
+    // Fallback.
+    int i = 0;
+    while ((x & 1) == 0) {
+        x >>= 1;
+        ++i;
+    }
+    return i
+#endif /* defined(__GNUC__) || defined(__clang__) */
+}
---
+++ src/utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+
+
+static inline int ctzll(unsigned long long x);
+
+
+
+#endif /* UTILS_H */

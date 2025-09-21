e4ad09204a7567b8128700ab38d740ce77af66c6
Pieter te Brake <pieterteb@gmail.com>
2025-09-21 23:34:51 +0200

    Some small showcasing tests.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "bitboard.h"
+#include "position.h"
 
 
 
 int main(int argc, char* argv[]) {
-    printf("Hello, world!\n");
+    for (int i = 1; i < argc; ++i)
+        printf("%s ", argv[0]);
+    printf("\n");
+
+    initialise_bitboards();
+
+    Position position = position_from_FEN("3Q4/1Q4Q1/4Q3/2Q4R/Q4Q2/3Q4/1Q4Rp/1K1BBNNk w - - 0 1");
+
+    
+    print_position(stdout, &position);
+
+    char* bitboard = bitboard_to_string(slider_attacks(PIECE_TYPE_BISHOP, SQUARE_E4, FILE_C_BITBOARD | RANK_5_BITBOARD), NULL);
+    printf("%s", bitboard);
+    free(bitboard);
 
     return 0;
 }

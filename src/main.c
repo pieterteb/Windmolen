a159ae17a1614f0fb1f5ac14049386ab18cd964b
Pieter te Brake <pieterteb@gmail.com>
2025-09-24 23:21:40 +0200

    Updated main.

--- src/main.c
+++ src/main.c
@@ -13,8 +13,7 @@ int main(int argc, char* argv[]) {
 
     initialise_bitboards();
 
-    Position position = position_from_FEN("3Q4/1Q4Q1/4Q3/2Q4R/Q4Q2/3Q4/1Q4Rp/1K1BBNNk w - - 0 1");
-
+    Position position = position_from_FEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     
     char* position_string = position_to_string(&position, NULL);
     printf("%s", position_string);

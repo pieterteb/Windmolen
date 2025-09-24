1a2546297bb6ab0db3d6f8b6f145b22998cda9c6
Pieter te Brake <pieterteb@gmail.com>
2025-09-24 21:47:27 +0200

    Function to create promotion move, and changes to MoveType enum.

--- src/move_generation.h
+++ src/move_generation.h
@@ -2,6 +2,8 @@
 #define MOVE_GENERATION_H
 
 
+#include <assert.h>
+#include <stdbool.h>
 #include <stdint.h>
 
 #include "types.h"
@@ -15,32 +17,23 @@
  * We use 16 bits to describe a move. We need 6 bits or the source square as well as the destination square. 
  * The remaining 4 bits are used for special moves:
  *
- *      Bits 0-5: source square,
- *      Bits 6-11: destination square,
- *      Bit 12: capture flag,
- *      Bit 13: promotion flag,
- *      Bit 14: special 0,
- *      Bit 15: special 1.
- * 
+ *      Bits 0-5: source square (0-63),
+ *      Bits 6-11: destination square (0-63),
+ *      Bits 12-13: promotion piece - 2,
+ *      Bits 14-15: special bits (promotion: 1, castle: 2, en passant: 3)
  */
 typedef uint16_t Move;
 
 typedef Move MoveType;
 enum MoveType {
-    MOVE_TYPE_QUIET =                       0b0000 << 12,
-    MOVE_TYPE_KING_CASTLE =                 0b1000 << 12,
-    MOVE_TYPE_QUEEN_CASTLE =                0b1100 << 12,
-    MOVE_TYPE_CAPTURE =                     0b0001 << 12,
-    MOVE_TYPE_EN_PASSANT_CAPTURE =          0b0101 << 12,
-    MOVE_TYPE_DOUBLE_PAWN_PUSH =            0b0100 << 12,
-    MOVE_TYPE_KNIGHT_PROMOTION =            0b0010 << 12,
-    MOVE_TYPE_BISHOP_PROMOTION =            0b0110 << 12,
-    MOVE_TYPE_ROOK_PROMOTION =              0b1010 << 12,
-    MOVE_TYPE_QUEEN_PROMOTION =             0b1110 << 12,
-    MOVE_TYPE_KNIGHT_PROMOTION_CAPTURE =    MOVE_TYPE_CAPTURE | MOVE_TYPE_KNIGHT_PROMOTION,
-    MOVE_TYPE_BISHOP_PROMOTION_CAPTURE =    MOVE_TYPE_CAPTURE | MOVE_TYPE_BISHOP_PROMOTION,
-    MOVE_TYPE_ROOK_PROMOTION_CAPTURE =      MOVE_TYPE_CAPTURE | MOVE_TYPE_ROOK_PROMOTION,
-    MOVE_TYPE_QUEEN_PROMOTION_CAPTURE =     MOVE_TYPE_CAPTURE | MOVE_TYPE_QUEEN_PROMOTION,
+    MOVE_TYPE_QUIET             =   0 << 14,
+    MOVE_TYPE_PROMOTION         =   1 << 14,
+    MOVE_TYPE_CASTLE            =   2 << 14,
+    MOVE_TYPE_EN_PASSANT        =   3 << 14,
+    MOVE_TYPE_KNIGHT_PROMOTION  =   MOVE_TYPE_PROMOTION | (0 << 12),
+    MOVE_TYPE_BISHOP_PROMOTION  =   MOVE_TYPE_PROMOTION | (1 << 12),
+    MOVE_TYPE_ROOK_PROMOTION    =   MOVE_TYPE_PROMOTION | (2 << 12),
+    MOVE_TYPE_QUEEN_PROMOTION   =   MOVE_TYPE_PROMOTION | (3 << 12),
 };
 
 static inline Square move_source(Move move) {
@@ -57,6 +50,12 @@ static inline Move new_move(Square from, Square to, MoveType type) {
     assert(is_valid_square(from) && is_valid_square(to));
 
     return from | (to << 6) | type;
+}
+
+static inline Move new_promotion(Square from, Square to, PieceType piece_type) {
+    assert(is_valid_square(from) && is_valid_square(to) && piece_type >= PIECE_TYPE_KNIGHT && piece_type <= PIECE_TYPE_QUEEN);
+
+    return new_move(from, to, (piece_type - 2) << 12);
 }
 
 

6806344be845268e37e59ee7e5d57ccd3ef60a0b
Pieter te Brake <pieterteb@gmail.com>
2025-11-04 18:20:09 +0100

    Revert time manager changes.

--- src/time_manager.c
+++ src/time_manager.c
@@ -22,11 +22,11 @@ void update_time_manager(struct TimeManager* time_manager, Color side_to_move) {
 
     if (time_manager->move_time > 0) {
         time_manager->cutoff_time = time_now + time_manager->move_time;
-    } else if (time_manager->moves_to_go > 0) {
-        time_manager->cutoff_time = time_now
-                                  + ((side_to_move == COLOR_WHITE) ? time_manager->white_time
-                                                                   : time_manager->black_time)
-                                    / time_manager->moves_to_go;
+    // } else if (time_manager->moves_to_go > 0) {
+    //     time_manager->cutoff_time = time_now
+    //                               + ((side_to_move == COLOR_WHITE) ? time_manager->white_time
+    //                                                                : time_manager->black_time)
+    //                                 / time_manager->moves_to_go;
     } else {
         const uint64_t white_cutoff_time = time_now + time_manager->white_time / 20 + time_manager->white_increment / 2;
         const uint64_t black_cutoff_time = time_now + time_manager->black_time / 20 + time_manager->black_increment / 2;
